0e5df8ba7f5accd8b1d58caca77fa664998696f4
Jiya <58591682+maryamsaleem108@users.noreply.github.com>
2022-11-16 15:11:23 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,20 +6,6 @@ class databaseConnect{
     public function __construct($host,$dbname,$username,$password){
         $connection = mysqli_connect($host,$username,$password,$dbname) or die("Connection Failed");
         $this->conn = $connection;
-//        $conn = new PDO('mysql:host = '.$host.';dbname ='.$dbname.';',$username,$password);
-//        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-//        $this->pdo = $conn;
     }
-
-//    public function query($q){
-//        $result = mysqli_query($this->conn,$q) or die("ERROR");
-////        $this->res = $result;
-//        return $result;
-//
-////        $statement = $this->pdo->prepare($q);
-////        $statement->execute();
-////        $data = $statement->fetchAll();
-////        return $data;
-//    }
 }
 ?>
